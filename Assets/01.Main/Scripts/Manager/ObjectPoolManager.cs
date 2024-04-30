5a1524875c878f72764cb87dc501ef62402195bd
SemiDragonDev <jypark323@naver.com>
2024-04-30 19:00:43 +0900

    20240430_19:00
    오브젝트풀매니저 수정중

--- Assets/01.Main/Scripts/Manager/ObjectPoolManager.cs
+++ Assets/01.Main/Scripts/Manager/ObjectPoolManager.cs
@@ -1,26 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class ObjectPoolManager : MonoBehaviour
 {
     public static ObjectPoolManager Instance;
 
+    public int defaultPoolSize = 5;
+    public int maxPoolSize = 10;
+
+    public IObjectPool<GameObject> Pool { get; private set; }
+
+    [SerializeField]
+    private List<GameObject> listOfPoolObjects = new List<GameObject>();
+
+    private Dictionary<GameObject, IObjectPool<GameObject>> poolDict = new Dictionary<GameObject, IObjectPool<GameObject>>();
+
     private void Awake()
     {
         if (Instance == null)
+        {
             Instance = this;
-        else
-            Destroy(this.gameObject);
+        }
+        else Destroy(this.gameObject);
     }
 
-    [System.Serializable]
-    private class ObjectPool
+    private void Init()
     {
-        public string objectName;
-        public GameObject prefab;
-        public int count;
+        Pool = new ObjectPool<GameObject>(CreatePooledObject, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, true, defaultPoolSize, maxPoolSize);
+
+        for(int i=0; i< listOfPoolObjects.Count; i++)
+        {
+            poolDict.Add(listOfPoolObjects[i], Pool);
+        }
     }
 
-    private Dictionary<string, Queue<GameObject>> poolDictionary = new Dictionary<string, Queue<GameObject>>();
+    private GameObject CreatePooledObject()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnTakeFromPool(GameObject @object)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnReturnedToPool(GameObject @object)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDestroyPoolObject(GameObject @object)
+    {
+        throw new NotImplementedException();
+    }
 }
