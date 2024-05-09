dcdf1898409bfc8c82825ed96682132d81084c9f
SemiDragonDev <jypark323@naver.com>
2024-05-09 19:37:26 +0900

    20240509_19:37

--- Assets/01.Main/Scripts/EnemyClass/FieldEnemy/Sort/Skeleton.cs
+++ Assets/01.Main/Scripts/EnemyClass/FieldEnemy/Sort/Skeleton.cs
@@ -7,9 +7,4 @@ public class Skeleton : FieldEnemy
     public override float Hp => base.Hp;
     public override float AttackDamage => base.AttackDamage;
     public override string Name => base.Name;
-
-    public override void SpawnAtNight(int spawnNum, string name)
-    {
-        base.SpawnAtNight(spawnNum, name);
-    }
 }
