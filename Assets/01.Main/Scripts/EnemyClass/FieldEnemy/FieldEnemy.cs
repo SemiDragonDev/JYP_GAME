6570a37cd8f95186f819cfd1d0e8cfa3b719e4cc
SemiDragonDev <jypark323@naver.com>
2024-05-09 19:43:14 +0900

    20240509_19:42
    FieldEnemy 클래스에서 SpawnAtNight 메서드 구상중

--- Assets/01.Main/Scripts/EnemyClass/FieldEnemy/FieldEnemy.cs
+++ Assets/01.Main/Scripts/EnemyClass/FieldEnemy/FieldEnemy.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FieldEnemy : Enemy
 {
     private float minDistToSpawn = 10f;
     private float maxDistToSpawn = 20f;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Vector3 playerPos;
 
     public virtual void SpawnAtNight(int spawnNum, string name)
     {
@@ -13,6 +16,8 @@ public class FieldEnemy : Enemy
         // 플레이어 중심 10유닛 보다는 멀리, 20유닛보다는 가까운 지역에 리스폰 되기로 지정
         // 5~10초 마다 거리 체크 후 30유닛보다 멀다면 풀에 return
         // return한 만큼 다시 지정된 지역에 랜덤 리스폰
+
+        NavMesh.SamplePosition(playerPos, out NavMeshHit hit, maxDistToSpawn, layerMask);
     }
 
     public void BurnAtDay()
