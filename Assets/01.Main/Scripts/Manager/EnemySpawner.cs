10231b748f411d9432f6d375798dae0784d32dfc
SemiDragonDev <jypark323@naver.com>
2024-05-10 19:39:31 +0900

    20240510_19:39

--- Assets/01.Main/Scripts/Manager/EnemySpawner.cs
+++ Assets/01.Main/Scripts/Manager/EnemySpawner.cs
@@ -18,12 +18,13 @@ public class EnemySpawner : MonoBehaviour
     // 그 위치의 x,z 값을 받고 y값 200 정도 위에서 아래 방향 Ray를 쏴 hit point를 받아온다.
     // 그 위치에 적 스폰시키기
 
-    public Vector3 GetRandomRayPointAroundPlayer()
+    public Vector3 GetRandomPointAroundPlayer()
     {
         playerPos = GameObject.FindGameObjectWithTag(playerTag).transform.position;
         Vector2 playerXZ = new Vector2(playerPos.x, playerPos.z);
         var randomPos = playerXZ + Random.insideUnitCircle * maxDistFromPlayer;
         var rayPos = new Vector3(randomPos.x, heightOfTheRay, randomPos.y);
+        //if(Physics.Raycast(rayPos, Vector3.down, ))
         return rayPos;
     }
 }
