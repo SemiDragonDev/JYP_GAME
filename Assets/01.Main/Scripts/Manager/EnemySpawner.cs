35e4a89d7d4b55764ad3fa96f6b0c91e9216066a
SemiDragonDev <jypark323@naver.com>
2024-05-13 16:04:43 +0900

    20240513_16:04
    N 입력시 20~50 반경 사이에 skeleton 한마리씩 생성까지 완료

--- Assets/01.Main/Scripts/Manager/EnemySpawner.cs
+++ Assets/01.Main/Scripts/Manager/EnemySpawner.cs
@@ -10,10 +10,17 @@ public class EnemySpawner : MonoBehaviour
     private Vector3 playerPos;
     private string playerTag = "Player";
 
-    private float minDistFromPlayer = 20f;
+    private float minDistFromPlayer = 30f;
     private float maxDistFromPlayer = 50f;
     private float heightOfTheRay = 200f;
     private float distBtwPlayerAndPoint;
+
+    private DayNightCycle dayNightCycle;
+
+    private void Awake()
+    {
+        dayNightCycle = GetComponent<DayNightCycle>();
+    }
 
     private void Update()
     {
@@ -23,14 +30,20 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
-    // 플레이어 반경 10unit~20unit 사이 Point를 random으로 받아오자.
+    // 플레이어 반경 30unit~50unit 사이 Point를 random으로 받아오자.
     // 그 위치의 x,z 값을 받고 y값 200 정도 위에서 아래 방향 Ray를 쏴 hit point를 받아온다.
     // 그 위치에 적 스폰시키기
 
     public void SpawnEnemy(string name)
     {
+        var gameObj = ObjectPool.Instance.GetPooledObject(name);
         playerPos = GameObject.FindGameObjectWithTag(playerTag).transform.position;
         Vector2 playerXZ = new Vector2(playerPos.x, playerPos.z);
+
+        //for (int i = 0; i < gameObj.defSize; i++) ;
+        //{
+
+        //}
         var randomPos = playerXZ + Random.insideUnitCircle * maxDistFromPlayer;
         distBtwPlayerAndPoint = (playerXZ - randomPos).magnitude;
         do
@@ -44,8 +57,9 @@ public class EnemySpawner : MonoBehaviour
         var rayPos = new Vector3(randomPos.x, heightOfTheRay, randomPos.y);
         if(Physics.Raycast(rayPos, Vector3.down, out RaycastHit hit, 300f, NavMesh.AllAreas))
         {
-            var gameObj = ObjectPool.Instance.GetPooledObject(name);
             gameObj.transform.position = hit.point;
         }
     }
+
+
 }
