2b8b0ba94fef7cb1818287cb4e2550d9b7dfdf4d
SemiDragonDev <jypark323@naver.com>
2024-05-14 18:52:08 +0900

    20240514_18:51

--- Assets/01.Main/Scripts/Manager/EnemySpawner.cs
+++ Assets/01.Main/Scripts/Manager/EnemySpawner.cs
@@ -10,6 +10,7 @@ public class EnemySpawner : MonoBehaviour
 
     private Vector3 playerPos;
     private string playerTag = "Player";
+    private string skeletonTag = "Skeleton";
 
     private float minDistFromPlayer = 30f;
     private float maxDistFromPlayer = 50f;
@@ -18,16 +19,18 @@ public class EnemySpawner : MonoBehaviour
 
     private List<GameObject> listOfGameObj = new List<GameObject>();
 
-    private void Awake()
+    private DayNightCycle dayNightCycle;
+
+    void Start()
     {
-        DayNightCycle dayNightCycle = GetComponent<DayNightCycle>();
+        
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.N))
+        if (dayNightCycle.isNight)
         {
-            SpawnEnemy("Skeleton");
+            SpawnEnemy(skeletonTag);
         }
     }
 
@@ -37,29 +40,35 @@ public class EnemySpawner : MonoBehaviour
 
     public void SpawnEnemy(string name)
     {
+        // 현재 Active 상태인 풀의 Object의 Count와
+        // 그 Object의 풀의 Default Size를 받아온다
         ObjectPool.Instance.CountActiveObjectsInList(name, out int count);
         int defaultSize = ObjectPool.Instance.GetDefSize(name);
 
         playerPos = GameObject.FindGameObjectWithTag(playerTag).transform.position;
         Vector2 playerXZ = new Vector2(playerPos.x, playerPos.z);
 
-        for(int i = 0; i < defaultSize; i++)
+        // 만약 Active 되어 있는 수가 Default Size보다 적은 경우 그 수만큼 Pool에서 불러와 스폰시킨다.
+        if (count < defaultSize)
         {
-            var randomPos = playerXZ + UnityEngine.Random.insideUnitCircle * maxDistFromPlayer;
-            distBtwPlayerAndPoint = (playerXZ - randomPos).magnitude;
-            var gameObj = ObjectPool.Instance.GetPooledObject(name);
-            do
+            for (int i = 0; i < defaultSize - count; i++)
             {
-                if (distBtwPlayerAndPoint < minDistFromPlayer)
-                {
-                    randomPos = playerXZ + UnityEngine.Random.insideUnitCircle * maxDistFromPlayer;
-                }
+                var randomPos = playerXZ + UnityEngine.Random.insideUnitCircle * maxDistFromPlayer;
                 distBtwPlayerAndPoint = (playerXZ - randomPos).magnitude;
-            } while (distBtwPlayerAndPoint < minDistFromPlayer);
-            var rayPos = new Vector3(randomPos.x, heightOfTheRay, randomPos.y);
-            if (Physics.Raycast(rayPos, Vector3.down, out RaycastHit hit, 500f, NavMesh.AllAreas))
-            {
-                gameObj.transform.position = hit.point;
+                var gameObj = ObjectPool.Instance.GetPooledObject(name);
+                do
+                {
+                    if (distBtwPlayerAndPoint < minDistFromPlayer)
+                    {
+                        randomPos = playerXZ + UnityEngine.Random.insideUnitCircle * maxDistFromPlayer;
+                    }
+                    distBtwPlayerAndPoint = (playerXZ - randomPos).magnitude;
+                } while (distBtwPlayerAndPoint < minDistFromPlayer);
+                var rayPos = new Vector3(randomPos.x, heightOfTheRay, randomPos.y);
+                if (Physics.Raycast(rayPos, Vector3.down, out RaycastHit hit, 500f, NavMesh.AllAreas))
+                {
+                    gameObj.transform.position = hit.point;
+                }
             }
         }
     }
