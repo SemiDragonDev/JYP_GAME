3960f5b0cfe7b9d622461db9df6243c7cce77bc5
SemiDragonDev <jypark323@naver.com>
2024-05-20 19:31:30 +0900

    Update Die.cs
    Die 수정중

--- Assets/01.Main/Scripts/FSM/EnemyState/Die.cs
+++ Assets/01.Main/Scripts/FSM/EnemyState/Die.cs
@@ -6,6 +6,7 @@ public class Die : FSMSingleton<Die>, IFSMState<EnemyStateManager>
 {
     public void OnEnter(EnemyStateManager e)
     {
+        // Death Animation 실행 후 아이템 드롭과 오브젝트 풀에 넣는 과정을 EnemyStateManager에 만들어서 이곳에서 실행하게 만들자
         e.animator.SetBool("IsDead", true);
     }
 
