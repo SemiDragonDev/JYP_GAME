6b18ed85405b496233f458ce452dde331d0f3681
SemiDragonDev <jypark323@naver.com>
2024-05-23 18:56:54 +0900

    cameraResolution 수정완료

--- Assets/01.Main/Scripts/Utility/CameraResolution.cs
+++ Assets/01.Main/Scripts/Utility/CameraResolution.cs
@@ -4,34 +4,30 @@ using UnityEngine;
 
 public class CameraResolution : MonoBehaviour
 {
-    Camera cam;
-    [SerializeField]
-    float fixedWidth = 1920f;
-    [SerializeField]
-    float fixedHeight = 1080f;
-    float fixedAspectRatio;
-
     private void Start()
     {
-        cam = GetComponent<Camera>();
-        fixedAspectRatio = fixedWidth / fixedHeight;
-        var curAspectRatio = (float)Screen.width / (float)Screen.height;
+        SetResolution();
+    }
 
-        if(curAspectRatio == fixedAspectRatio)
+    public void SetResolution()
+    {
+        int setWidth = 1920;
+        int setHeight = 1080;
+
+        int deviceWidth = Screen.width;
+        int deviceHeight = Screen.height;
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true);
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight)
         {
-            cam.rect = new Rect(0f, 0f, 1f, 1f);
+            float newWidth = ((float)setWidth / setHeight) * ((float)deviceWidth / deviceHeight);
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f);
         }
-        else if(curAspectRatio > fixedAspectRatio)
+        else
         {
-            float width = fixedAspectRatio / curAspectRatio;
-            float x = (1f - width) / 2f;
-            cam.rect = new Rect(x, 0f, width, 1f);
-        }
-        else if(curAspectRatio < fixedAspectRatio)
-        {
-            float height = curAspectRatio / fixedAspectRatio;
-            float y = (1f - height) / 2f;
-            cam.rect = new Rect(0f, y, 1f, height);
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight);
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight);
         }
     }
 }
