50fe58c61371eadacce7b1229edb7d69c288cd6d
SemiDragon <jypark323@naver.com>
2024-05-26 18:31:41 +0900

    Update Die.cs
    Burn VFX 를 다시 Pool로 돌아가게하는 작업중

--- Assets/01.Main/Scripts/FSM/EnemyState/Die.cs
+++ Assets/01.Main/Scripts/FSM/EnemyState/Die.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Die : FSMSingleton<Die>, IFSMState<EnemyStateManager>
@@ -24,6 +25,9 @@ public class Die : FSMSingleton<Die>, IFSMState<EnemyStateManager>
         passedTime = 0f;
         e.animator.SetBool("IsDead", false);
         ObjectPool.Instance.ReturnToPool(e.pooledObject);
+        //var vfxObj = transform.Find("BurnEffect(Clone)");
+        //var vfxPooledObj = vfxObj.GetComponent<PooledObject>();
+        //ObjectPool.Instance.ReturnToPool(vfxPooledObj);
         e.ResetHp();
     }
 }
