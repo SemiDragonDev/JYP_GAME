ad356ab4915ea1694d83dd60ceb6027467760b3e
SemiDragonDev <jypark323@naver.com>
2024-05-31 19:43:07 +0900

    ..
    TakeDamage 까지 완료
    
    낮에 Burn 복구할 차례

--- Assets/01.Main/Scripts/Interact/Attackable.cs
+++ Assets/01.Main/Scripts/Interact/Attackable.cs
@@ -4,14 +4,17 @@ public class Attackable : MonoBehaviour, IInteractable
 {
     KnockBack knockback;
     Vector3 vfxPosition = new Vector3(0f, 0.9f, 0f);
+    Enemy enemy;
 
     public void Interact()
     {
         Debug.Log("Attacked: " + gameObject.name);
 
         knockback = GetComponent<KnockBack>();
+        enemy = GetComponent<Enemy>();
         knockback.PlayingKnockBack();
         var vfxObj = ObjectPool.Instance.GetPooledObject("HitEffect").gameObject;
         vfxObj.transform.position = this.transform.position + vfxPosition;
+        enemy.TakeDamage(50f);
     }
 }
