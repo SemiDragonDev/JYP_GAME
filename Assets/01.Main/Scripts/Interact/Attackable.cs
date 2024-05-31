12a33b906fb018a165f261fa3b5490d96f5ef454
SemiDragonDev <jypark323@naver.com>
2024-05-31 19:52:46 +0900

    Update Attackable.cs
    HitColorChange 추가했는데
    오류 생성...
    수정 필요

--- Assets/01.Main/Scripts/Interact/Attackable.cs
+++ Assets/01.Main/Scripts/Interact/Attackable.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Attackable : MonoBehaviour, IInteractable
@@ -5,6 +6,8 @@ public class Attackable : MonoBehaviour, IInteractable
     KnockBack knockback;
     Vector3 vfxPosition = new Vector3(0f, 0.9f, 0f);
     Enemy enemy;
+    Renderer objRenderer;
+    Color originColor;
 
     public void Interact()
     {
@@ -16,5 +19,16 @@ public class Attackable : MonoBehaviour, IInteractable
         var vfxObj = ObjectPool.Instance.GetPooledObject("HitEffect").gameObject;
         vfxObj.transform.position = this.transform.position + vfxPosition;
         enemy.TakeDamage(50f);
+        //StartCoroutine(HitColorChange());
+    }
+
+    public IEnumerator HitColorChange()
+    {
+        objRenderer = GetComponent<Renderer>();
+        originColor = objRenderer.material.color;
+
+        objRenderer.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        objRenderer.material.color = originColor;
     }
 }
