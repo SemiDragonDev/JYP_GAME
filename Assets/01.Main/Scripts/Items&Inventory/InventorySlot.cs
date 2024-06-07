9899bd804bd896a70c638245757ac54b4c81c34a
SemiDragonDev <jypark323@naver.com>
2024-06-07 10:37:19 +0900

    ..

--- Assets/01.Main/Scripts/Items&Inventory/InventorySlot.cs
+++ Assets/01.Main/Scripts/Items&Inventory/InventorySlot.cs
@@ -1,14 +1,18 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using static UnityEditor.Progress;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour, IPointerClickHandler
 {
     public Image iconImage;
     public TextMeshProUGUI stackSizeText;
 
     public InventoryItem Item {  get;  set; }
+
+    private static InventorySlot pickedSlot = null;
+    private static Image draggingItemIcon;
 
     public void AddItem(InventoryItem newInventoryItem)
     {
@@ -42,29 +46,96 @@ public class InventorySlot : MonoBehaviour
         }
     }
 
-    public void SetItem(Item newItem, int count)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        Item.item = newItem;
-        if (newItem != null)
+        if (pickedSlot == null)
         {
-            iconImage.sprite = newItem.icon;
-            iconImage.enabled = true;
-
-            if (count > 1)
-            {
-                stackSizeText.text = count.ToString();
-                stackSizeText.enabled = true;
-            }
-            else
-            {
-                stackSizeText.enabled = false;
-            }
+            // Select the current slot and start dragging
+            pickedSlot = this;
+            StartDragging();
         }
         else
         {
-            iconImage.sprite = null;
-            iconImage.enabled = false;
+            // Swap items between pickedSlot and current slot
+            SwapItems(pickedSlot, this);
+            // End dragging
+            EndDragging();
+            // Deselect the slot
+            pickedSlot = null;
+        }
+    }
+
+    // Function to start dragging the item icon
+    private void StartDragging()
+    {
+        if (Item != null)
+        {
+            // Use the existing dragging icon
+            draggingItemIcon.sprite = iconImage.sprite;
+            draggingItemIcon.raycastTarget = false; // Make sure the icon doesn't block raycasts
+            draggingItemIcon.gameObject.SetActive(true);
+        }
+    }
+
+    private void EndDragging()
+    {
+        if (draggingItemIcon != null)
+        {
+            draggingItemIcon.gameObject.SetActive(false);
+            draggingItemIcon.raycastTarget = true;
+        }
+    }
+
+    // Function to swap items between two slots
+    private void SwapItems(InventorySlot slot1, InventorySlot slot2)
+    {
+        var tempItem = slot1.Item;
+        slot1.Item = slot2.Item;
+        slot2.Item = tempItem;
+
+        // Update both slots
+        slot1.UpdateSlot();
+        slot2.UpdateSlot();
+    }
+
+    public void UpdateSlot()
+    {
+        if (Item != null)
+        {
+            iconImage.sprite = Item.item.icon;
+            // Assuming you have an Image component to display the icon in the slot
+            GetComponent<Image>().sprite = iconImage.sprite;
+            GetComponent<Image>().enabled = true;
+
+            stackSizeText.text = Item.stackSize > 1 ? Item.stackSize.ToString() : "";
+            stackSizeText.enabled = true;
+        }
+        else
+        {
+            GetComponent<Image>().enabled = false;
             stackSizeText.enabled = false;
         }
     }
+
+    private void Start()
+    {
+        UpdateSlot();
+
+        // Find the dragging icon in the cursor object
+        if (draggingItemIcon == null)
+        {
+            draggingItemIcon = GameObject.Find("DraggingItemIcon").GetComponent<Image>();
+            draggingItemIcon.gameObject.SetActive(false); // Initially inactive
+        }
+    }
+
+    void Update()
+    {
+        // Update the dragging icon position every frame
+        if (draggingItemIcon != null && draggingItemIcon.gameObject.activeSelf)
+        {
+            // Position is handled by the CursorFollower script
+        }
+    }
+
 }
